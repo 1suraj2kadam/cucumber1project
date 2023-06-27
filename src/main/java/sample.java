9036ab17fb6a53657a575077a63325e99f276e2b
ACER <ACER@DESKTOP-I5E1K8B.mshome.net>
2023-06-27 17:48:44 +0530

    method inputed

--- src/main/java/sample.java
+++ src/main/java/sample.java
@@ -1,4 +1,12 @@
 
 public class sample {
-
+		int i=2;
+		public static void main(String[] args) {
+			sample t=new sample();
+			t.sure();
+		}
+		
+		public void sure() {
+			System.out.println(i);
+		}
 }
